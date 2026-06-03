cde21492e71b8f95fe0d66d9843a722b2efbb74d
Denys Liapin <denys.liapin@gmail.com>
2026-06-03 16:58:19 +0300

    Selecting a patient ID has been added
    The possibility of selecting a patient by ID has been added

--- BC_grapher.R
+++ BC_grapher.R
@@ -1,13 +1,32 @@
 library(ggplot2)
 
-file_name <- commandArgs(trailingOnly = T)
+arg <- commandArgs(trailingOnly = T)
 
+# checking the number of arguments
+if (length(arg) < 2) {
+  stop ("Missing arguments! Usage: Rscript BC_grapher.R \"<file_name>.csv\" <Patient_ID>")
+}
+if (length(arg) > 2) {
+  stop ("Too many arguments! If your file name has spaces, enclose it in quotes: \"My file.csv\"")
+}
+
+file_name <- arg[1]
+id <- arg[2]
 data <- read.csv2(file_name, stringsAsFactors = FALSE)
-data_clean <- subset(data, Parameter != "" & !is.na(Parameter))
+data_patient <- subset(data, as.character(Patient_ID) == as.character(id))
+
+# checking the presence of the patient's ID
+if (nrow(data_patient) == 0) {
+  stop(paste0("Patient with ID '", id, "' not found in this file!"))
+}
+
+# data preparation
+data_clean <- subset(data_patient, Parameter != "" & !is.na(Parameter))
 data_clean$Date <- as.Date(data_clean$Date, format = "%d.%m.%Y")
 data_clean$Value <- as.numeric(data_clean$Value)
 unique_parameters <- unique(data_clean$Parameter)
 
+# graph preparation
 for (par in unique_parameters) {
   par_data <- subset(data_clean, Parameter == par)
   par_data <- par_data[order(par_data$Date), ]
@@ -23,6 +42,6 @@ for (par in unique_parameters) {
          x = "Date",
          y = "Value")
   
-  file_name <- paste0(par, "_graph.png")
-  ggsave(filename = file_name, plot = p, width = 7, height = 5, dpi = 300)
+  final_file_name <- paste0(id, "_", par, "_graph.png")
+  ggsave(filename = final_file_name, plot = p, width = 7, height = 5, dpi = 300)
 }
